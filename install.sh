ca16a64dc96740713e8815986141c3c03d55781b
Patrick Gatewood <pg8wood@vt.edu>
2019-10-02 13:54:26 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,66 @@
+# Inspired by this awesome setup script: https://github.com/vsbuffalo/dotfiles/blob/master/setup.sh 
+
+RCol='\033[0m'
+Gre='\033[0;32m'
+Red='\033[0;31m'
+Yel='\033[0;33m'
+
+# ~~~~~~~~~~~~~~~~~~
+# Printing functions 
+# ~~~~~~~~~~~~~~~~~~
+function gecho {
+  echo "${Gre}[message] $1${RCol}"
+}
+
+function yecho {
+  echo "${Yel}[warning] $1${RCol}"
+}
+
+function wecho {
+  # red, but don't exit 1
+  echo "${Red}[error] $1${RCol}"
+}
+
+
+function recho {
+  echo "${Red}[error] $1${RCol}"
+  exit 1
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Look for command line tool, if not install via homebrew
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function install_brew {
+  (command -v $1 > /dev/null  && gecho "$1 found...") || 
+    (yecho "$1 not found, installing via homebrew..." && brew install $1)
+}
+
+
+function linkdotfile {
+  file="$1"
+  if [ ! -L ~/$file ]; then 
+    yecho "$file found, but it's not ours. Moving it to /tmp"
+    mv ~/$1 /tmp
+  fi
+
+  if [ ! -e ~/$file -a ! -L ~/$file ]; then
+      yecho "linking $file... " >&2
+      ln -s ~/dotfiles/$file ~/$file
+  else
+      gecho "ignoring $file because it's already linked" >&2
+  fi
+}
+
+
+# Installation actions
+install_brew npm
+npm install --global pure-prompt
+
+linkdotfile .aliasrc
+linkdotfile .bashrc
+linkdotfile .dir_colors
+linkdotfile .git
+linkdotfile .gitconfig
+linkdotfile .gitignore_global
+linkdotfile .vimrc
+linkdotfile .zshrc
